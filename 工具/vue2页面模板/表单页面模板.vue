225cf234f8c7c015a96ba41d1069e574551ccf49
wuhaoran <wuhaoran@gigaiot.com>
2023-11-22 20:02:30 +0800

    1

--- 工具/vue2页面模板/表单页面模板.vue
+++ 工具/vue2页面模板/表单页面模板.vue
@@ -1,74 +1,66 @@
-<template>
-  <!-- 表单页面模板 -->
-  <!-- 记得引入样式 -->
-  <!-- <com-edit
-      v-if="editVisible"
+<!-- 表单页面模板 -->
+<!-- 留意样式引入和国际化 -->
+<!-- <com-edit
+      v-if="showEdit"
       :close="
         () => {
-          editVisible = false
+          showEdit = false
         }
       "
       :data="{ x: 1 }"
     ></com-edit> -->
+<template>
   <div>
-    <el-dialog title="表单" visible="visible" width="30%" :before-close="close">
+    <el-dialog :title="$t('表单')" visible="visible" width="30%" :before-close="close">
       <el-form :model="form" :rules="rulesRef" ref="formRef" label-width="80px">
-        <el-form-item label="名称" prop="name">
-          <el-input v-model="form.name" placeholder="请输入名称" />
+        <el-form-item :label="$t('名称')" prop="name">
+          <el-input v-model="form.name" :placeholder="$t('请输入名称')" />
         </el-form-item>
 
-        <el-form-item label="备注" prop="desc">
+        <el-form-item :label="$t('备注')" prop="desc">
           <el-input v-model="form.desc" />
         </el-form-item>
 
-        <el-form-item label="数量" prop="number">
+        <el-form-item :label="$t('数量')" prop="number">
           <el-input-number :min="0" v-model="form.number" />
         </el-form-item>
 
-        <el-form-item label="类型" prop="type">
+        <el-form-item :label="$t('类型')" prop="type">
           <el-select v-model="form.type">
             <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"> </el-option>
           </el-select>
         </el-form-item>
 
-        <el-form-item label="是否" prop="yes">
+        <el-form-item :label="$t('是否')" prop="yes">
           <el-radio-group v-model="form.yes" size="small">
-            <el-radio-button :label="1">是</el-radio-button>
-            <el-radio-button :label="0">否</el-radio-button>
+            <el-radio-button :label="1">{{ $t("是") }}</el-radio-button>
+            <el-radio-button :label="0">{{ $t("否") }}</el-radio-button>
           </el-radio-group>
         </el-form-item>
 
-        <el-form-item label="时间" prop="time">
-          <el-date-picker
-            v-model="form.time"
-            type="daterange"
-            range-separator="至"
-            start-placeholder="开始日期"
-            end-placeholder="结束日期"
-          >
-          </el-date-picker>
+        <el-form-item :label="$t('时间')" prop="time">
+          <el-date-picker v-model="form.time" type="daterange" :range-separator="$t('至')" :start-placeholder="$t('开始日期')" :end-placeholder="$t('结束日期')"> </el-date-picker>
         </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <div class="f">
           <div class="fa"></div>
-          <el-button @click="close">取 消</el-button>
-          <el-button type="primary" @click="onSubmit">确 定</el-button>
+          <el-button @click="close">{{ $t("取消") }}</el-button>
+          <el-button type="primary" @click="onSubmit">{{ $t("确定") }}</el-button>
         </div>
       </span>
     </el-dialog>
   </div>
 </template>
 <script>
-import '@/assets/css/baseStyle.css'
+import "@/assets/css/baseStyle.css";
+// import {  } from "@/utils/baseTool";
+// import {  } from '@/api/'
+
 export default {
   props: {
-    close: {
-      type: Function
-    },
-    data: {
-      type: Object
-    }
+    close: { type: Function },
+    data: { type: Object },
   },
   data() {
     return {
@@ -78,46 +70,52 @@ export default {
           {
             required: true,
             validator: async (_rule, value) => {
-              if (value === '' || !value) {
-                throw new Error('此项必填')
-              }
-            }
-          }
+              if (value === "" || !value) throw new Error(this.$t("此项必填"));
+            },
+          },
         ],
-        desc: []
+        desc: [],
       },
       options: [
-        {
-          label: '类型1',
-          value: 1
-        },
-        {
-          label: '类型2',
-          value: 2
-        }
+        { label: "类型1", value: 1 },
+        { label: "类型2", value: 2 },
       ],
       form: {
-        name: '',
-        desc: '',
+        name: "",
+        desc: "",
         number: 0,
         type: 1,
         yes: 0,
-        time: []
-      }
-    }
+        time: [],
+      },
+    };
   },
+  computed: {
+    com1() {
+      return "";
+    },
+  },
+  // watch: {
+  //   form: {
+  //     handler: function (n, o) {
+  //       // console.log(n);
+  //     },
+  //     immediate: true,
+  //     deep: true,
+  //   },
+  // },
   methods: {
     async onSubmit() {
-      console.log('onSubmit')
-      console.log(this.form)
-      this.$refs['formRef'].validate((valid) => {
-        console.log(valid)
-      })
-    }
+      console.log("onSubmit");
+      console.log(this.form);
+      this.$refs["formRef"].validate((valid) => {
+        console.log(valid);
+      });
+    },
   },
   mounted() {
-    console.log(this.data)
-  }
-}
+    console.log(this.data);
+  },
+};
 </script>
 <style scoped></style>
--- 工具/vue2页面模板/表格页面模板.vue
+++ 工具/vue2页面模板/表格页面模板.vue
@@ -1,3 +1,5 @@
+<!-- 表格页面模板 -->
+<!-- 留意样式引入和国际化 -->
 <template>
   <div style="margin: 24px; position: relative">
     <!-- 表单 -->
@@ -5,32 +7,29 @@
       <el-form :inline="true">
         <!-- 输入框 -->
         <el-form-item>
-          <el-input v-model="x"> </el-input>
+          <el-input v-model="form.x" :placeholder="$t('请输入')"> </el-input>
         </el-form-item>
         <!-- 选择器 -->
         <el-form-item>
-          <el-select v-model="x">
-            <el-option v-for="item in x" :key="item" :label="item.label" :value="item.value"></el-option>
+          <el-select v-model="form.y">
+            <!-- <el-option v-for="item in x" :key="item" :label="item.label" :value="item.value"></el-option> -->
           </el-select>
         </el-form-item>
         <!-- 时间段 -->
         <el-form-item>
-          <el-date-picker
-            type="daterange"
-            v-model="date"
-            range-separator="-"
-            start-placeholder="开始时间"
-            end-placeholder="结束时间"
-          >
-          </el-date-picker>
+          <el-date-picker type="daterange" v-model="date" range-separator="-" :start-placeholder="$t('开始时间')" :end-placeholder="$t('结束时间')"> </el-date-picker>
+        </el-form-item>
+        <!-- 新建 -->
+        <el-form-item>
+          <el-button type="primary" @click="create">{{ $t("新建") }}</el-button>
         </el-form-item>
         <!-- 查询 -->
         <el-form-item>
-          <el-button type="primary" @click="init">{{ '查询' }}</el-button>
+          <el-button type="primary" @click="init">{{ $t("查询") }}</el-button>
         </el-form-item>
         <!-- 重置 -->
         <el-form-item>
-          <el-button type="primary" @click="reset">{{ '重置' }}</el-button>
+          <el-button type="primary" @click="reset">{{ $t("重置") }}</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -38,16 +37,22 @@
     <el-card shadow="never" style="margin-bottom: 15px" :body-style="{ padding: '15px' }">
       <el-table row-key="id" :data="list" v-loading="loading">
         <!-- 标题 -->
-        <el-table-column :label="'标题'" prop="userId"> </el-table-column>
+        <el-table-column :label="$t('标题')" prop="userId"> </el-table-column>
+        <!-- 内容 -->
+        <el-table-column :label="$t('内容')">
+          <template #default="{ row }">
+            {{ row }}
+          </template>
+        </el-table-column>
         <!-- 操作 -->
-        <el-table-column label="操作" prop="userId" align="center" min-width="90">
+        <el-table-column :label="$t('操作')" prop="userId" align="center" min-width="90">
           <template #default="{ row }">
             <!-- 详情 -->
-            <el-button type="text" @click="clickDet(row)">{{ '详情' }}</el-button>
+            <el-button type="text" @click="toDet(row)">{{ $("详情") }}</el-button>
             <!-- 修改 -->
-            <el-button type="text" @click="clickEdit(row)">{{ '修改' }}</el-button>
+            <el-button type="text" @click="toEdit(row)">{{ $("修改") }}</el-button>
             <!-- 删除 -->
-            <el-button type="text" @click="clickDel(row)">{{ '删除' }}</el-button>
+            <el-button type="text" @click="toDel(row)">{{ $("删除") }}</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -65,64 +70,109 @@
         </el-pagination>
       </div>
     </el-card>
+    <!-- 表单 -->
+    <!-- <com-edit
+      v-if="showEdit"
+      :close="
+        () => {
+          showEdit = false;
+          reset();
+        }
+      "
+      :data="editData"
+    ></com-edit> -->
   </div>
 </template>
 <script>
-import '@/assets/css/baseStyle.css'
-import {} from '@/utils/baseTool'
+import "@/assets/css/baseStyle.css";
+// import {  } from "@/utils/baseTool";
+// import comEdit from "./components/edit.vue";
 // import {  } from '@/api/'
 
 export default {
   props: {},
+  // components: { comEdit },
   data() {
     return {
-      x: '',
+      // showEdit: false,
+      // editData: null,
       loading: false,
       list: [],
+      form: {},
       pagination: {
         total: 0,
         current: 1,
-        pageSize: 10
+        pageSize: 10,
       },
-      date: null
-    }
+      date: null,
+    };
+  },
+  computed: {
+    com1() {
+      return "";
+    },
+  },
+  watch: {
+    form: {
+      handler: function (n, o) {
+        // console.log(n);
+      },
+      immediate: true,
+      deep: true,
+    },
   },
   methods: {
     // 初始化
     init() {
-      this.list.length = 0
-      this.loading = true
+      this.list.length = 0;
+      this.loading = true;
       const params = {
         page: this.pagination.current,
         size: this.pagination.pageSize,
-        timeZone: new Date().getTimezoneOffset() / -60
-      }
-      api(params)
-        .then((res) => {
-          console.log(res)
-          // 如果这里报错，分页器会消失
-          this.list = res.data
-          this.pagination.total = res.total
-        })
-        .finally(() => {
-          this.loading = false
-        })
+        timeZone: new Date().getTimezoneOffset() / -60,
+        ...this.form,
+      };
+      console.log(params);
+      // api(params)
+      //   .then((res) => {
+      //     console.log(res);
+      //     // 如果这里报错，分页器会消失
+      //     this.list = res.data;
+      //     this.pagination.total = res.total;
+      //   })
+      //   .finally(() => {
+      //     this.loading = false;
+      //   });
     },
+    // 新建
+    create() {
+      // this.showEdit = true;
+    },
+    // 详情
+    toDet(row) {},
+    // 编辑
+    toEdit(row) {
+      // this.editData = row;
+      // this.showEdit = true;
+    },
+    // 删除
+    toDel(row) {},
     // 重置
     reset() {
-      this.pagination.current = 1
-      this.date = [new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), new Date()]
-      this.init()
+      this.form = {};
+      this.pagination.current = 1;
+      this.date = [new Date(new Date().getTime() - 7 * 24 * 60 * 60 * 1000), new Date()];
+      this.init();
     },
     // 下一页
     nextPage(p) {
-      this.pagination.current = p
-      this.init()
-    }
+      this.pagination.current = p;
+      this.init();
+    },
   },
   mounted() {
-    this.reset()
-  }
-}
+    this.reset();
+  },
+};
 </script>
 <style scoped></style>
