22fe1d3aeca2a0335eca054477424acefacc6c4c
wuhaoran <1173816413@qq.com>
2024-03-31 19:46:53 +0800

    1

--- 工具/项目功能/版本更新/App.vue
+++ 工具/项目功能/版本更新/App.vue
@@ -2,13 +2,15 @@
 export default {
   created() {
     // 检查版本更新 不一致就刷新
-    this.intervalTime = setInterval(() => {
+    const getVersion = () => {
       uni.request({ url: `${window.location.origin}/version.json`, method: "get" }).then((r) => {
         if (!r || !r[1]) return;
         // console.log(this.$version, r[1].data.version);
         if (r[1].statusCode && r[1].statusCode === 200 && this.$version != r[1].data.version) window.location.reload();
       });
-    }, 5000);
+    };
+    getVersion();
+    this.intervalTime = setInterval(getVersion, 10000);
     this.$on("hook:beforeDestroy", () => {
       clearInterval(this.intervalTime);
       this.intervalTime = null;
