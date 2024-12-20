ca5278c00c7028f56d0a81ac45563096966cfb7a
whr <1173816413@qq.com>
2024-12-20 09:55:46 +0800

    ol

--- openlayers/ol/src/components/ol2.vue
+++ openlayers/ol/src/components/ol2.vue
@@ -2,15 +2,9 @@
   <!-- ol封装 -->
   <div class="w100p h100p">
     <div id="mapDom" class="w100p h100p"></div>
-    <div id="overlayDom1" class="hp w40 h40 br50p fc c0 fz16 fw6" style="background: #8f1d22; box-shadow: 0px 2px 4px 0px rgb(0, 0, 0, 0.4); border: 1.5px solid; transform: translate(-50%, -50%)">
-      {{ d.value1 }}
-    </div>
-    <div id="overlayDom2" class="hp w40 h40 br50p fc c0 fz16 fw6" style="background: #003460; box-shadow: 0px 2px 4px 0px rgb(0, 0, 0, 0.4); border: 1.5px solid; transform: translate(-50%, -50%)">
-      {{ d.value2 }}
-    </div>
     <div id="tipDom" class="br4 pt12 pb12 pl20 pr20 fy bc0" style="min-width: 200px">
       <div class="lh24 fw6 mb4" @click="go">标题</div>
-      <div class="lh24 mb4 fz14">内容</div>
+      <div class="lh24 mb4 fz14">{{ d.value }}</div>
       <div class="lh24 fz14" style="color: #999">底部</div>
     </div>
   </div>
@@ -18,35 +12,22 @@
 
 <script setup>
 import { ref, reactive, onMounted } from 'vue';
-import * as mapTool from '@/utils/gis/ol';
+import olMap from '@/utils/gis/ol';
 
 const d = reactive({
-  value1: 1,
-  value2: 2,
+  value: 1,
 });
 
 const initMap1 = () => {
   const pos1 = [113.37310399318999, 23.12297649456611];
   const pos2 = [113.36309041374084, 23.121647418783482];
   const options = { dom: 'mapDom', position: pos1 };
-  mapTool.initMap(options); // 初始化地图
-  const overlayDom1 = mapTool.addOverlay('overlayDom1', pos1); // 添加标点1
-  console.log('overlayDom1', overlayDom1);
-  const overlayDom2 = mapTool.addOverlay('overlayDom2', pos2); // 添加标点2
-  const overlaysList = mapTool.getOverlays();
-  mapTool.delOverlays(overlaysList[1]); // 删除标点2
-};
-
-const initMap2 = () => {
-  const pos1 = [113.37310399318999, 23.12297649456611];
-  const pos2 = [113.36309041374084, 23.121647418783482];
-  const options = { dom: 'mapDom', position: pos1 };
-  mapTool.initMap(options); // 初始化地图
-  mapTool.addLayer(pos1); // 添加标点1
-  mapTool.addLayer(pos2); // 添加标点2
-  mapTool.pointermove();
-  const tipDom = mapTool.addOverlay('tipDom', null, 'bottom'); // 添加提示弹窗
-  mapTool.singleclick(tipDom); // 添加点击标点：点击出现提示弹窗
+  const map = new olMap(options); // 初始化地图
+  map.addLayer(pos1); // 添加标点1
+  map.addLayer(pos2); // 添加标点2
+  map.pointermove(); // 鼠标变手
+  const tipDom = map.addOverlay('tipDom', null, 'bottom'); // 添加提示弹窗
+  map.singleclick(tipDom); // 添加点击标点：点击出现提示弹窗
 };
 
 const go = () => {
@@ -54,8 +35,7 @@ const go = () => {
 };
 
 onMounted(() => {
-  // initMap1();
-  initMap2();
+  initMap1();
 });
 </script>
 
--- openlayers/ol/src/utils/gis/ol.js
+++ openlayers/ol/src/utils/gis/ol.js
@@ -15,62 +15,64 @@ import { defaults as defaultInteractions, DragRotateAndZoom } from 'ol/interacti
 
 import ico from './ico.ico';
 
-let map = null;
-
-// 初始化地图
-export const initMap = (options) => {
-  map = new Map({
-    target: options.dom,
-    layers: [new Tile({ source: new XYZ({ url: 'https://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}' }) })],
-    view: new View({ projection: 'EPSG:4326', center: options.position, minZoom: 0, maxZoom: 18, zoom: 15, constrainResolution: true }),
-    controls: defaults().extend([new FullScreen(), new MousePosition(), new ScaleLine()]),
-    interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
-  });
-};
-
-// 添加标点
-export const addLayer = (position) => {
-  let feature = new Feature({ geometry: new Point(position) });
-  feature.setStyle([new Style({ image: new Circle({ radius: 12, stroke: new Stroke({ color: '#fff' }), fill: new Fill({ color: '#003460' }) }) })]); // 添加标点
-  // feature.setStyle([new Style({ image: new Icon({ anchor: [0, 0], src: ico, offset: [0, 0] }) })]); // 添加图片
-  let source = new VectorSource({});
-  source.addFeatures([feature]);
-  let vector = new VectorLayer({ source });
-
-  map.addLayer(vector);
-};
-// 添加overlay
-export const addOverlay = (id, position, positioning, offset = [0, 0]) => {
-  const marker = new Overlay({
-    element: document.getElementById(id),
-    positioning,
-    position: position ? fromLonLat(position, 'EPSG:4326') : null,
-    offset,
-    autoPan: true,
-    stopEvent: false,
-  });
-  map.addOverlay(marker);
-  return marker;
-};
-
-// 删除overlay
-export const delOverlays = (overlays) => map.removeOverlay(overlays);
-
-// 获取overlay列表
-export const getOverlays = () => map.getOverlays().array_;
-
-// 点击feature显示弹窗
-export const singleclick = (overlay) => {
-  map.on('singleclick', (e) => {
-    let feature = map.forEachFeatureAtPixel(e.pixel, (feature) => feature);
-    if (feature) overlay.setPosition(feature.getGeometry().getCoordinates());
-    else overlay.setPosition(undefined);
-  });
-};
-// 悬浮feature添加样式
-export const pointermove = () => {
-  map.on('pointermove', (e) => {
-    if (map.hasFeatureAtPixel(e.pixel)) map.getViewport().style.cursor = 'pointer';
-    else map.getViewport().style.cursor = 'inherit';
-  });
-};
+export default class olMap {
+  constructor(options) {
+    this.initMap(options);
+  }
+  // 初始化地图
+  initMap(options) {
+    this.map = new Map({
+      target: options.dom,
+      layers: [new Tile({ source: new XYZ({ url: 'https://webrd01.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}' }) })],
+      view: new View({ projection: 'EPSG:4326', center: options.position, minZoom: 0, maxZoom: 18, zoom: 15, constrainResolution: true }),
+      controls: defaults().extend([new FullScreen(), new MousePosition(), new ScaleLine()]),
+      interactions: defaultInteractions().extend([new DragRotateAndZoom()]),
+    });
+  }
+  // 添加标点
+  addLayer(position) {
+    const feature = new Feature({ geometry: new Point(position) });
+    feature.setStyle([new Style({ image: new Circle({ radius: 12, stroke: new Stroke({ color: '#fff' }), fill: new Fill({ color: '#003460' }) }) })]); // 添加标点
+    // feature.setStyle([new Style({ image: new Icon({ anchor: [0, 0], src: ico, offset: [0, 0] }) })]); // 添加图片
+    const source = new VectorSource({});
+    source.addFeatures([feature]);
+    const vector = new VectorLayer({ source });
+    this.map.addLayer(vector);
+  }
+  // 添加overlay
+  addOverlay(id, position, positioning, offset = [0, 0]) {
+    const marker = new Overlay({
+      element: document.getElementById(id),
+      positioning,
+      position: position ? fromLonLat(position, 'EPSG:4326') : null,
+      offset,
+      autoPan: true,
+      stopEvent: false,
+    });
+    this.map.addOverlay(marker);
+    return marker;
+  }
+  // 删除overlay
+  delOverlays(overlays) {
+    return this.map.removeOverlay(overlays);
+  }
+  // 获取overlay列表
+  getOverlays() {
+    return this.map.getOverlays().array_;
+  }
+  // 点击feature显示弹窗
+  singleclick(overlay) {
+    this.map.on('singleclick', (e) => {
+      const feature = this.map.forEachFeatureAtPixel(e.pixel, (feature) => feature);
+      if (feature) overlay.setPosition(feature.getGeometry().getCoordinates());
+      else overlay.setPosition(undefined);
+    });
+  }
+  // 悬浮feature添加样式
+  pointermove() {
+    this.map.on('pointermove', (e) => {
+      if (this.map.hasFeatureAtPixel(e.pixel)) this.map.getViewport().style.cursor = 'pointer';
+      else this.map.getViewport().style.cursor = 'inherit';
+    });
+  }
+}
