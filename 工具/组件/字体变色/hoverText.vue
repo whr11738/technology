6a822e128e6318a23ff2f156b14ca729e0a03f22
whr <1173816413@qq.com>
2025-02-19 14:14:05 +0800

    字体变色组件

---
+++ 工具/组件/字体变色/hoverText.vue
@@ -0,0 +1,54 @@
+<!-- 鼠标移入变色的文字组件 -->
+<!-- html
+ <hoverText class="br4" fc="red" bc="#0000ff" :hold="false">
+  <div class="fc fz16 lh22 fw6 hp">Hover over me!</div>
+</hoverText>
+ -->
+<!-- js
+import hoverText from '@/components/hoverText.vue';
+ -->
+
+<template>
+  <div :style="styleObject" @mouseenter="enter" @mouseleave="leave">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    fc: { type: String, default: '#fff' }, // 字体颜色
+    bc: { type: String, default: '#000' }, // 背景颜色
+    hold: { type: Boolean, default: false }, // 是否保持hover状态
+  },
+  data() {
+    return {
+      hover: false,
+    };
+  },
+  watch: {
+    hold(newValue) {
+      this.hover = newValue;
+    },
+  },
+  computed: {
+    styleObject() {
+      return {
+        transition: 'color 0.3s, background-color 0.3s',
+        color: this.hover ? this.fc : 'initial',
+        backgroundColor: this.hover ? this.bc : 'initial',
+      };
+    },
+  },
+  methods: {
+    enter() {
+      if (!this.hold) this.hover = true;
+    },
+    leave() {
+      if (!this.hold) this.hover = false;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
